fc1c4477ca0c0af4db383cf828d4a26158eff9f8
Celezt <celezt@github.com>
2020-12-01 21:29:57 +0100

    added last- child and parent function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,25 @@
+// https://github.com/SimonSapin/rust-forest
 // https://github.com/RazrFalcon/rctree/blob/master/src/lib.rs
 
 use std::cell::{Cell, Ref, RefCell, RefMut};
 use std::rc::{Rc, Weak};
 
+thread_local! {static ID_COUNT: i32 = 0;}
+
+/// Mutable reference.
 type Link<T, U> = Rc<RefCell<Node<T, U>>>;
+/// Weak mutable reference.
+type WeakLink<T, U> = Weak<RefCell<Node<T, U>>>;
+
+/// Return value if `Some`, else return `None`.
+macro_rules! try_opt {
+    ($expr: expr) => {
+        match $expr {
+            Some(value) => value,
+            None => return None,
+        }
+    };
+}
 
 pub struct DT<T, U>(Link<T, U>)
 where
@@ -15,10 +31,13 @@ where
     U: Eq + PartialEq,
 {
     children: Vec<Link<T, U>>,
+    last_parent: Option<WeakLink<T, U>>,
+    last_child: Option<Link<T, U>>,
     decision: U,
     data: T,
 }
 
+/// Cloning a 'Node' only increments a reference count. It does not copy the data.
 impl<T, U: Eq> Clone for DT<T, U> {
     fn clone(&self) -> Self {
         DT(Rc::clone(&self.0))
@@ -58,16 +77,37 @@ impl<T, U> DT<T, U>
 where
     U: Eq + PartialEq,
 {
-    /// Create new instance of a node
+    /// Create new instance of a node.
     pub fn new(data: T, decision: U) -> DT<T, U> {
         DT(Rc::new(RefCell::new(Node {
             children: Vec::new(),
+            last_parent: None,
+            last_child: None,
             decision: decision,
             data: data,
         })))
     }
+    /// Returns the amount of children that node contains.
     pub fn child_len(&self) -> usize {
         self.0.borrow().children.len()
+    }
+    /// Return a reference to the latest parent node.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the node is currently mutably borrowed.
+    pub fn last_parent(&self) -> Option<DT<T, U>> {
+        Some(DT(try_opt!(
+            try_opt!(self.0.borrow().last_parent.as_ref()).upgrade()
+        )))
+    }
+    /// Return a reference to the latest child node.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the node is currently mutably borrowed.
+    pub fn last_child(&self) -> Option<DT<T, U>> {
+        Some(DT(try_opt!(self.0.borrow().last_child.as_ref()).clone()))
     }
     /// Borrow an immutable reference
     pub fn borrow(&self) -> Ref<T> {
@@ -82,18 +122,19 @@ where
     /// # Panics
     ///
     /// Panics if the node tries to append to itself.
-    pub fn append(&mut self, new_child: DT<T, U>) {
+    pub fn append(&mut self, new_child: DT<T, U>) -> DT<T, U> {
         assert!(*self != new_child, "Not legal to append to itself");
 
         // Borrow the reference
         let mut self_borrow = self.0.borrow_mut();
-        /* {
-            let mut new_child_borrow = new_child.0.borrow_mut();
-        } */
+        let mut new_child_borrow = new_child.0.borrow_mut();
+        // Borrow a reference of the latest parent (this)
+        new_child_borrow.last_parent = Some(Rc::downgrade(&self.0));
+        // Borrow a reference of the latest child (new_child)
+        self_borrow.last_child = Some(new_child.0.clone());
 
-        self_borrow.children.push(new_child.0);
+        self_borrow.children.push(new_child.0.clone());
 
-        //new_child_borrow
+        self.clone()
     }
-    pub fn remove(&mut self) {}
 }
--- tests/test_dt.rs
+++ tests/test_dt.rs
@@ -9,9 +9,37 @@ mod tests {
         println!("{:#?}", root);
         println!("{:#?}", node);
         println!("{}", root.child_len());
-        root.append(node);
+
+        root.append(node.clone());
+        println!("{}", node.last_parent().unwrap());
+
         println!("{}", root.child_len());
-        root.append(DT::new("data3", true));
+
+        root.append(DT::new("data3", true))
+            .last_child()
+            .unwrap()
+            .append(DT::new("child1", true))
+            .append(DT::new("child2", false))
+            .last_child()
+            .unwrap()
+            .append(DT::new("child3", true))
+            .append(DT::new("child4", false))
+            .last_parent()
+            .unwrap()
+            .last_parent()
+            .unwrap()
+            .append(DT::new("data4", false))
+            .append(DT::new("data5", false));
+
         println!("{}", root.child_len());
+
+        println!("{:#?}", root.last_child().unwrap());
+    }
+
+    #[test]
+    #[should_panic(expected = "Not legal to append to itself")]
+    fn test_overwrite_itself() {
+        let mut root = DT::new("root", true);
+        root.append(root.clone());
     }
 }
